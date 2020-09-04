ca9e121bcacf1766ed0c5ef1796932c00501009c
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-04 12:31:14 -0400

    added logger, MVP

---
+++ middleware/logger.js
@@ -0,0 +1,6 @@
+const logger = (req, res, next) => {
+    console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`)
+    next()
+}
+
+module.exports = logger
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const server = express();
 
+const helmet = require('helmet')
 
 const projectsRouter = require('./routes/projects-router')
 const actionsRouter = require('./routes/actions-router')
-// import custom logger middleware
+const logger = require('./middleware/logger')
 
-// server.use helmet
-// server.use logger
+
+server.use(logger())
+server.use(helmet())
 server.use(express.json())
 server.use('/api/projects', projectsRouter)
 server.use('/api/actions', actionsRouter)
